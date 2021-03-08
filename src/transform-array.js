8247cac62de1f5674bba8f94d6d6e35f839896ad
Yura <ytezher@gmail.com>
2021-03-09 00:25:13 +0300

    fiat:decision what-season,hanoi-tower

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,34 @@
 module.exports = function transform( arr ) {
   if(!Array.isArray(arr)){
-      return Error();
+      throw Error();
   }
+   const arrNew = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case "--discard-next":
+                i++;
+                break;
+            case "--discard-prev":
+                if (arrNew.length !== 0 && arr[i - 2] !== '--discard-next') {
+                    arrNew.pop();
+                }
+                break;
+            case "--double-next":
+                if (i !== arr.length - 1) {
+                    arrNew.push(arr[i + 1]);
+                }
+                break;
+            case "--double-prev":
+                if (arrNew.length !== 0 && arr[i - 2] !== '--discard-next') {
+                    arrNew.push(arr[i - 1])
+                }
+                break;
+            default:
+                arrNew.push(arr[i])
+                break;
+        }
+    }
+    return arrNew;
+
 };
